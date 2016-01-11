8aabe5f22aa251cbe13efd114d08519b9d34ac9f
Alex Branham <branham@utexas.edu>
2016-01-11 15:04:51 -0500

    Finalized SPSA wordcloud R file

--- code/spsa-2016-wordcloud.R
+++ code/spsa-2016-wordcloud.R
@@ -2,6 +2,7 @@ library(twitteR)
 library(tm)
 library(wordcloud)
 library(RColorBrewer)
+library(stringr)
 
 
 consumer_key <- Sys.getenv("TWITTER_API_KEY")
@@ -12,6 +13,8 @@ setup_twitter_oauth(consumer_key, consumer_secret)
 SPSA <- searchTwitter("#SPSA2016", n = 1500,
                       since = "2016-01-06", until = "2016-01-09")
 
+save(SPSA, file = "../data/SPSA-2016-tweets.R")
+
 spsa_text <- sapply(SPSA, function(x) x$getText())
 
 spsa_corpus <- Corpus(VectorSource(spsa_text))
@@ -19,6 +22,21 @@ spsa_corpus <- Corpus(VectorSource(spsa_text))
 spsa_corpus<- tm_map(spsa_corpus, content_transformer(tolower))
 spsa_corpus <- tm_map(spsa_corpus, removePunctuation)
 spsa_corpus <- tm_map(spsa_corpus, function(x)removeWords(x,stopwords()))
+spsa_corpus <- tm_map(spsa_corpus, function(x)removeWords(x, "spsa2016"))
 
 pal2 <- brewer.pal(8,"Dark2")
-wordcloud(spsa_corpus,min.freq=2,max.words=150, random.order=FALSE, colors=pal2)
+
+jpeg("../image/spsa-wordcloud.jpeg")
+wordcloud(spsa_corpus, min.freq = 3,
+          random.order = FALSE, colors = pal2)
+dev.off()
+
+hashtags <- unlist(str_extract_all(spsa_text, "#\\w+"))
+hashtags_freq <- table(hashtags)
+conference_tags <- c("#SPSA2016", "#spsa2016")
+
+jpeg("../image/spsa-hashtags.jpeg")
+wordcloud(names(hashtags_freq[!names(hashtags_freq) %in% conference_tags]),
+          hashtags_freq[!names(hashtags_freq) %in% conference_tags],
+          random.order = FALSE, min.freq = 2, colors = pal2)
+dev.off()
